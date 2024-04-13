f8a4c64fa019bdaf63bddf87ce002cff032d1940
Arsen Shulika <arsenshulika@gmail.com>
2024-04-13 22:59:51 +0200

    commit css i html

--- src/js/FAQ.js
+++ src/js/FAQ.js
@@ -1,28 +1,6 @@
 'use strict';
 
-// import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
-document.addEventListener('DOMContentLoaded', function () {
-  const accordionItems = document.querySelectorAll('.faq');
-
-  accordionItems.forEach(item => {
-    const header = item.querySelector('.faq-question-container');
-    const content = item.querySelector('.faq-answer-container');
-    const arrowIcon = header.querySelector(
-      '.faq-btn-accordion>.faq-btn-accordion>.faq-arrow-icon'
-    );
-
-    header.addEventListener('click', function () {
-      item.classList.toggle('active');
-
-      if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-        arrowIcon.classList.remove('active');
-      } else {
-        content.style.maxHeight = content.scrollHeight + 'px';
-        arrowIcon.classList.add('active');
-      }
-    });
-  });
-});
+new Accordion('.accordion-container');
